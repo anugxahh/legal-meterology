08c02a01c79003bc06937d74f772e119155f60af
Avfubuu1 <nandagopan.harinarayan@gmail.com>
2026-09-01 15:43:04 +0530

    Add ScheduleRepository interface for Schedule entity

--- legal-meterology/src/main/java/com/example/legal_meterology/repository/ScheduleRepository.java
+++ legal-meterology/src/main/java/com/example/legal_meterology/repository/ScheduleRepository.java
@@ -1,1 +1,7 @@
+package com.example.legal_meterology.repository;
 
+import com.example.legal_meterology.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+}
