9d79adc526bb2ee566005333fc291cc82a2d4723
Avfubuu1 <nandagopan.harinarayan@gmail.com>
2026-09-02 19:54:31 +0530

    Remove @Repository annotation from InstrumentRepository
    Removed the @Repository annotation from InstrumentRepository.

--- legal-meterology/src/main/java/com/example/legal_meterology/repository/InstrumentRepository.java
+++ legal-meterology/src/main/java/com/example/legal_meterology/repository/InstrumentRepository.java
@@ -2,9 +2,8 @@ package com.example.legal_meterology.repository;
 
 import com.example.legal_meterology.entity.Instrument;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
-@Repository
 public interface InstrumentRepository extends JpaRepository<Instrument, UUID> {
 }
