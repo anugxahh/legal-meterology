e2d5ac58110ab760f14b237fed5d386d61e2042d
Avfubuu1 <nandagopan.harinarayan@gmail.com>
2026-09-02 19:56:20 +0530

    Update VerificationApplicationRepository.java

--- legal-meterology/src/main/java/com/example/legal_meterology/repository/VerificationApplicationRepository.java
+++ legal-meterology/src/main/java/com/example/legal_meterology/repository/VerificationApplicationRepository.java
@@ -1,6 +1,7 @@
 package com.example.legal_meterology.repository;
 
 import com.example.legal_meterology.entity.VerificationApplication;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
